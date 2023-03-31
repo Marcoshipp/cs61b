cca88f694160aad527527a3b8d8a658d333ddd33
Marcoshipp <marcobelesay@gmail.com>
2023-03-31 12:00:55 +0800

    completed project1a probably going to submit for now

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,109 +1,129 @@
 public class ArrayDeque<T> {
     private int size;
-    private int nextFirst;
-    private int nextLast;
+    private int front;
+    private int back;
     private T[] items;
-    private static final int RFACTOR = 2;
-    private double R;
+    private int length;
 
     public ArrayDeque() {
         this.size = 0;
-        this.nextFirst = 0;
-        this.nextLast = 1;
-        this.R = 0;
-        this.items = (T[]) new Object[8];
+        this.front = this.size + 1;
+        this.back = this.size + 2;
+        this.length = 8;
+        this.items = (T[]) new Object[this.length];
     }
 
-    private void resize(int capacity) {
-        T[] a = (T[]) new Object[capacity];
-        System.arraycopy(this.items, 0, a, 0, this.size);
+    private int addOne(int x) {
+        if (x == this.length - 1) {
+            return 0;
+        }
+        return x + 1;
+    }
+
+    private int minusOne(int x) {
+        if (x == 0) {
+            return this.length - 1;
+        }
+        return x - 1;
+    }
+
+    private void resizeArr(int flag) {
+        T[] a;
+        if (flag == -1) {
+            a = (T[]) new Object[this.length / 2];
+        }
+        else {
+            a = (T[]) new Object[this.length * 2];
+        }
+        int arrIndex = 0;
+        int p1 = addOne(this.front);
+        int p2 = minusOne(this.back);
+        // copying from items to new array
+        while (p1 != p2) {
+            a[arrIndex] = this.items[p1];
+            p1 = addOne(p1);
+            arrIndex++;
+        }
+        a[arrIndex] = this.items[p1];
+        // resetting all pointers
+        if (flag == -1) {
+            this.length /= 2;
+        }
+        else {
+            this.length *= 2;
+        }
         this.items = a;
-        this.nextFirst = this.size;
-        this.nextLast = this.nextFirst + 1;
+        this.front = this.length - 1;
+        this.back = this.size;
     }
 
-    private void downSize() {
-
-    }
-
-    public void addFirst(T item) {
-        if (this.size == this.items.length) {
-            resize(size * RFACTOR);
-        }
-        this.items[nextFirst] = item;
-        if (nextFirst == 0) {
-            nextFirst = size;
-        }
-        else {
-            nextFirst--;
-        }
-        size++;
-        this.R = (double) this.size / this.items.length;
-    }
-
-    public void addLast(T item) {
-        if (this.size == this.items.length) {
-            resize(size * RFACTOR);
-        }
-        this.items[nextLast] = item;
-        if (nextLast + 1 == size) {
-            nextLast = 0;
-        }
-        else {
-            nextLast++;
-        }
-        size++;
-        this.R = (double) this.size / this.items.length;
-    }
-
-    // done
     public boolean isEmpty() {
         return this.size == 0;
     }
 
-    // done
-    public void printDeque() {
-        for (T t : this.items) {
-            System.out.print(t + " ");
-        }
-        System.out.println();
-    }
-
-    private int first(int nextFirst) {
-        if (nextFirst + 1 == this.size) {
-            return 0;
-        }
-        return nextFirst + 1;
-    }
-
-    private int last(int nextLast) {
-        if (nextLast + 1 == this.size) {
-            return 0;
-        }
-        return nextLast + 1;
-    }
-
-    // do this
-    public T removeFirst() {
-        this.nextFirst = first(nextFirst);
-        T r = this.items[nextFirst];
-        this.items[nextFirst] = null;
-        this.R = (double) this.size / this.items.length;
-        return r;
-    }
-
-    // do this
-    public T removeLast() {
-        this.R = (double) this.size / this.items.length;
-        return null;
-    }
-
-    // done
-    public T get(int index) {
-        if (index >= this.size) {
-            return null;
-        }
-        return this.items[index];
+    public int size() {
+        return this.size;
     }
     
+    public void printDeque() {
+        int p1 = addOne(this.front);
+        int p2 = minusOne(this.back);
+        while (p1 != p2) {
+            System.out.print(this.items[p1] + " ");
+            p1 = addOne(p1);
+        }
+        System.out.println(this.items[p2]);
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= this.size) {
+            return null;
+        }
+        int p = addOne(this.front);
+        while (index != 0) {
+            p = addOne(p);
+            index--;
+        }
+        return this.items[p];
+    }
+
+    public void addFirst(T item) {
+        if (this.size == this.length) {
+            resizeArr(1);
+        }
+        this.items[this.front] = item;
+        this.front = minusOne(this.front);
+        this.size++;
+    }
+
+    public void addLast(T item) {
+        if (this.size == this.length) {
+            resizeArr(1);
+        }
+        this.items[this.back] = item;
+        this.back = addOne(this.back);
+        this.size++;
+    }
+
+    public T removeFirst() {
+        this.front = addOne(this.front);
+        T returnItem = this.items[this.front];
+        this.items[this.front] = null;
+        this.size--;
+        if (this.length >= 16 && this.size / (double) this.length < 0.25) {
+            resizeArr(-1);
+        }
+        return returnItem;
+    }
+
+    public T removeLast() {
+        this.back = minusOne(this.back);
+        T returnItem = this.items[this.back];
+        this.items[this.back] = null;
+        this.size--;
+        if (this.length >= 16 && this.size / (double) this.length < 0.25) {
+            resizeArr(-1);
+        }
+        return returnItem;
+    }
 }
--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,147 +1,104 @@
 public class LinkedListDeque<T> {
-    private class DLList {
-        private IntNode sentinal;
-        private int size;
-    
-        private class IntNode {
-            public IntNode prev;
-            public T item;
-            public IntNode next;
-    
-            public IntNode(IntNode p, T x, IntNode n) {
-                this.prev = p;
-                this.item = x;
-                this.next = n;
-            }
-        }
-    
-        public DLList() {
-            this.sentinal = new IntNode(this.sentinal, null, this.sentinal);
-            this.sentinal.next = this.sentinal;
-            this.sentinal.prev = this.sentinal.next;
-            this.size = 0;
-        }
-    
-        public void insertFront(T x) {
-            IntNode p = new IntNode(this.sentinal, x, this.sentinal.next);
-            this.sentinal.next.prev = p;
-            this.sentinal.next = p;
-            this.size++;
-        }
-    
-        public void insertBack(T x) {
-            IntNode p = new IntNode(this.sentinal.prev, x, this.sentinal);
-            this.sentinal.prev.next = p;
-            this.sentinal.prev = p;
-            this.size++;
-        }
-    
-        public T get(int index) {
-            IntNode p = this.sentinal.next;
-            while (p != this.sentinal && index != 0) {
-                p = p.next;
-                index--;
-            }
-            return p.item;
-        }
+    public class IntNode {
+        public IntNode prev;
+        public T item;
+        public IntNode next;
 
-        private T getRecursive(int index, IntNode p) {
-            if (index == 0) {
-                if (p.prev != this.sentinal) {
-                    return null;
-                }
-                return p.next.item;
-            }
-            return getRecursive(index - 1, p.next);
-        }
-
-        public T getRecursive(int index) {
-            return getRecursive(index, this.sentinal);
-        }
-        
-        public T removeFront() {
-            if (this.sentinal.next == this.sentinal) {
-                return null;
-            }
-            IntNode r = this.sentinal.next;
-            this.sentinal.next.next.prev = this.sentinal;
-            this.sentinal.next = this.sentinal.next.next;
-            this.size--;
-            return r.item;
-        }
-
-        public T removeBack() {
-            if (this.sentinal.prev == this.sentinal) {
-                return null;
-            }
-            IntNode r = this.sentinal.prev;
-            this.sentinal.prev.prev.next = this.sentinal;
-            this.sentinal.prev = this.sentinal.prev.prev;
-            this.size--;
-            return r.item;
-        }
-
-        public void printList() {
-            IntNode p = this.sentinal.next;
-            while (p != this.sentinal) {
-                if (p.next == this.sentinal) {
-                    System.out.print(p.item + "\n");
-                }
-                else {
-                    System.out.print(p.item + " ");
-                }
-                p = p.next;
-            }
-        }
-    
-        public int size() {
-            return this.size;
-        }
-
-        public boolean isEmpty() {
-            return size() == 0;
+        public IntNode(IntNode p, T x, IntNode n) {
+            this.prev = p;
+            this.item = x;
+            this.next = n;
         }
     }
-
-    private DLList deque;
-
+    
+    private IntNode sentinal;
+    private int size;
     public LinkedListDeque() {
-        this.deque = new DLList();
+        this.sentinal = new IntNode(this.sentinal, null, this.sentinal);
+        this.sentinal.next = this.sentinal;
+        this.sentinal.prev = this.sentinal.next;
+        this.size = 0;
     }
 
     public void addFirst(T item) {
-        this.deque.insertFront(item);
+        IntNode p = new IntNode(this.sentinal, item, this.sentinal.next);
+        this.sentinal.next.prev = p;
+        this.sentinal.next = p;
+        this.size++;
     }
 
     public void addLast(T item) {
-        this.deque.insertBack(item);
+        IntNode p = new IntNode(this.sentinal.prev, item, this.sentinal);
+        this.sentinal.prev.next = p;
+        this.sentinal.prev = p;
+        this.size++;
     }
 
     public boolean isEmpty() {
-        return this.deque.isEmpty();
+        return this.size == 0;
     }
 
     public int size() {
-        return this.deque.size();
+        return this.size;
     }
 
     public void printDeque() {
-        this.deque.printList();
+        IntNode p = this.sentinal.next;
+        while (p != this.sentinal) {
+            if (p.next == this.sentinal) {
+                System.out.print(p.item + "\n");
+            }
+            else {
+                System.out.print(p.item + " ");
+            }
+            p = p.next;
+        }
     }
 
     public T removeFirst() {
-        return this.deque.removeFront();
+        if (this.sentinal.next == this.sentinal) {
+            return null;
+        }
+        IntNode r = this.sentinal.next;
+        this.sentinal.next.next.prev = this.sentinal;
+        this.sentinal.next = this.sentinal.next.next;
+        this.size--;
+        return r.item;
     }
-
+    
     public T removeLast() {
-        return this.deque.removeBack();
+        if (this.sentinal.prev == this.sentinal) {
+            return null;
+        }
+        IntNode r = this.sentinal.prev;
+        this.sentinal.prev.prev.next = this.sentinal;
+        this.sentinal.prev = this.sentinal.prev.prev;
+        this.size--;
+        return r.item;
     }
 
     public T get(int index) {
-        return this.deque.get(index);
+        IntNode p = this.sentinal.next;
+        while (p != this.sentinal && index != 0) {
+            p = p.next;
+            index--;
+        }
+        return p.item;
+    }
+
+    private T getRecursive(int index, IntNode n) {
+        if (index == 0) {
+            return n.item;
+        }
+        return getRecursive(index - 1, n.next);
     }
 
     public T getRecursive(int index) {
-        return this.deque.getRecursive(index);
+        if (index >= this.size || index < 0) {
+            return null;
+        }
+        return getRecursive(index, this.sentinal.next);
     }
+
 }
+
